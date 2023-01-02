7ba55c7053aa52ccd77163729b00fe595dd345b5
calebyyy <caleb.ng.yy@gmail.com>
2023-01-02 19:05:38 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
-import Navbar from '../Components/Navbar'
-import '../styles/globals.css'
+import Navbar from '../components/Navbar';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <>
-  <Navbar />
-   <Component {...pageProps} />
-   </>
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
